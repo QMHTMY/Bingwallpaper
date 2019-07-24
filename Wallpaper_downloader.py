7cf1a3a173ad5717f22c2af29607de02c2ad8fd9
shieber <QMH_XB_FLTMY@yahoo.com>
2019-07-24 23:00:51 +0800

    modify file

--- Wallpaper_downloader.py
+++ Wallpaper_downloader.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+#    Author: Shieber
+#    Date: 2019.07.16
+#
+#                             APACHE LICENSE
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+#                            Function Description
+#    一键下载Bing（必应）的每日壁纸并免除掉水印。
+#
+#    Copyright 2019 
+#    All Rights Reserved!
 
 import re 
 import sys
